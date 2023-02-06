0c7793526eb7933edb23454bd81cd80d3e590a73
Wilco Louwerse <wilco@conduction.nl>
2023-02-06 12:00:45 +0100

    Remove zgw prefixes from endpoints

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -523,6 +523,54 @@ class InstallationService implements InstallerInterface
 
         return $endpoints;
     }
+    
+    /**
+     * Update all existing zgw endpoints and remove any prefixes
+     *
+     * @return void
+     */
+    private function cleanZgwEndpointPrefixes()
+    {
+        (isset($this->io)?$this->io->writeln(['','<info>Removing ZGW endpoint prefixes</info>']):'');
+    
+        $collections = $this->entityManager->getRepository('App:CollectionEntity')->findBy(['plugin' => 'ZgwBundle']);
+        (isset($this->io)?$this->io->writeln('Found '.count($collections).' Collections'):'');
+        
+        foreach ($collections as $collection) {
+            (isset($this->io)?$this->io->writeln("Removing prefix {$collection->getPrefix()}") : '');
+            $this->removeEntityEndpointsPrefix($collection);
+            (isset($this->io)?$this->io->newLine() : '');
+        }
+    }
+    
+    /**
+     * Remove prefixes from zgw endpoints, loop through all entities of a collection and remove prefix from all connected endpoints
+     *
+     * @return void
+     */
+    private function removeEntityEndpointsPrefix(CollectionEntity $collection)
+    {
+        foreach ($collection->getEntities() as $entity) {
+            if (!$endpoints = $this->entityManager->getRepository('App:Endpoint')->findBy(['entity' => $entity])) {
+                (isset($this->io)?$this->io->writeln(["No endpoint found for entity: {$entity->getName()}"]):'');
+                continue;
+            }
+            (isset($this->io)?$this->io->writeln("Found ".count($endpoints)." endpoint(s) for : {$entity->getName()}, start removing prefix") : '');
+            foreach ($endpoints as $endpoint) {
+                // Update pathRegex, removing prefix
+                $endpoint->setPathRegex(str_replace($collection->getPrefix().'/', '', $endpoint->getPathRegex()));
+            
+                // Count how many items we need to remove from the path array, by exploding prefix on '/'
+                $explodedPrefix = explode('/', $collection->getPrefix());
+                $arrayItemsCount = count($explodedPrefix);
+                // Update path for this endpoint, removing the prefix
+                $endpoint->setPath(array_slice($endpoint->getPath(), $arrayItemsCount));
+            
+                $this->entityManager->persist($endpoint);
+                (isset($this->io)?$this->io->writeln("Updated endpoint {$endpoint->getName()}, prefix removed") : '');
+            }
+        }
+    }
 
     private function addSchemasToCollection(CollectionEntity $collection, string $schemaPrefix): CollectionEntity
     {
@@ -594,6 +642,9 @@ class InstallationService implements InstallerInterface
         // Create Sources & proxy endpoints
         $this->createSources($this::SOURCES);
         $this->createProxyEndpoints($this::PROXY_ENDPOINTS);
+        
+        // Clean up prefixes from all ZGW endpoints
+        $this->cleanZgwEndpointPrefixes();
 
         // Lets see if there is a generic search endpoint
 
