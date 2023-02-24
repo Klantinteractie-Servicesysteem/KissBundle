c42690e07c3ad2d2eef15abef429c2bbbf66125c
Wilco Louwerse <wilco@conduction.nl>
2023-02-24 16:51:15 +0100

    moved dashboardCards to installation.json

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -22,12 +22,13 @@ class InstallationService implements InstallerInterface
     private ContainerInterface $container;
     private SymfonyStyle $io;
 
-    public const OBJECTS_THAT_SHOULD_HAVE_CARDS = [
-        'https://kissdevelopment.commonground.nu/kiss.openpubSkill.schema.json',
-        'https://kissdevelopment.commonground.nu/kiss.resultaatypeomschrijvinggeneriek.schema.json',
-        'https://kissdevelopment.commonground.nu/kiss.link.schema.json',
-        'https://kissdevelopment.commonground.nu/kiss.afdelingsnaam.schema.json'
-    ];
+    // todo: remove this if installation.json version works
+//    public const OBJECTS_THAT_SHOULD_HAVE_CARDS = [
+//        'https://kissdevelopment.commonground.nu/kiss.openpubSkill.schema.json',
+//        'https://kissdevelopment.commonground.nu/kiss.resultaatypeomschrijvinggeneriek.schema.json',
+//        'https://kissdevelopment.commonground.nu/kiss.link.schema.json',
+//        'https://kissdevelopment.commonground.nu/kiss.afdelingsnaam.schema.json'
+//    ];
 
     public const SCHEMAS_THAT_SHOULD_HAVE_ENDPOINTS = [
         ['reference' => 'https://kissdevelopment.commonground.nu/kiss.openpubSkill.schema.json',                 'path' => 'ref/openpub_skill',                    'methods' => []],
@@ -605,35 +606,36 @@ class InstallationService implements InstallerInterface
         (isset($this->io) ? $this->io->writeln(count($collections).' Collections Created'): '');
         return $collections;
     }
-
-    public function createDashboardCards($objectsThatShouldHaveCards)
-    {
-        foreach ($objectsThatShouldHaveCards as $object) {
-            (isset($this->io) ? $this->io->writeln('Looking for a dashboard card for: ' . $object) : '');
-            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
-            if (
-                !$dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entityId' => $entity->getId()])
-            ) {
-                $dashboardCard = new DashboardCard();
-                $dashboardCard->setType('schema');
-                $dashboardCard->setEntity('App:Entity');
-                $dashboardCard->setObject('App:Entity');
-                $dashboardCard->setName($entity->getName());
-                $dashboardCard->setDescription($entity->getDescription());
-                $dashboardCard->setEntityId($entity->getId());
-                $dashboardCard->setOrdering(1);
-                $this->entityManager->persist($dashboardCard);
-                (isset($this->io) ? $this->io->writeln('Dashboard card created') : '');
-                continue;
-            }
-            (isset($this->io) ? $this->io->writeln('Dashboard card found') : '');
-        }
-    }
+    
+    // todo: remove this if installation.json version works
+//    public function createDashboardCards($objectsThatShouldHaveCards)
+//    {
+//        foreach ($objectsThatShouldHaveCards as $object) {
+//            (isset($this->io) ? $this->io->writeln('Looking for a dashboard card for: ' . $object) : '');
+//            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
+//            if (
+//                !$dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entityId' => $entity->getId()])
+//            ) {
+//                $dashboardCard = new DashboardCard();
+//                $dashboardCard->setType('schema');
+//                $dashboardCard->setEntity('App:Entity');
+//                $dashboardCard->setObject('App:Entity');
+//                $dashboardCard->setName($entity->getName());
+//                $dashboardCard->setDescription($entity->getDescription());
+//                $dashboardCard->setEntityId($entity->getId());
+//                $dashboardCard->setOrdering(1);
+//                $this->entityManager->persist($dashboardCard);
+//                (isset($this->io) ? $this->io->writeln('Dashboard card created') : '');
+//                continue;
+//            }
+//            (isset($this->io) ? $this->io->writeln('Dashboard card found') : '');
+//        }
+//    }
 
     public function checkDataConsistency(){
-
-        // Lets create some genneric dashboard cards
-        $this->createDashboardCards($this::OBJECTS_THAT_SHOULD_HAVE_CARDS);
+        // todo: remove this if installation.json version works
+//        // Lets create some genneric dashboard cards
+//        $this->createDashboardCards($this::OBJECTS_THAT_SHOULD_HAVE_CARDS);
 
         $this->createCollections();
 
